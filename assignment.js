4642327fcbef814afc4f9778f595492ff213907e
Satadru Roy <satadru.roy98@gmail.com>
2020-07-02 15:12:04 +0530

    Task 2 complete

--- assignment.js
+++ assignment.js
@@ -1,6 +1,19 @@
-//////////////////////// Task 1 complete
 const sayHello = name => {
   console.log('Hi ' + name);
 }
 
+////////////////////// Task 2 complete
+const sayHello1 = (name, phrase) => {
+  console.log(`${phrase} ${name}`);
+}
+
+const sayHello2 = () => {
+  console.log('Hi Satx!');
+}
+
+const sayHello3 = name => 'Hi ' + name;
+
 sayHello('Satx');
+sayHello1('Satx', 'Hello');
+sayHello2();
+console.log(sayHello3('Satx'));
