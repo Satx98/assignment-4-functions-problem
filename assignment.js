ace2603f53bec70e36b904c0593e00cd7d8f6f78
Satadru Roy <satadru.roy98@gmail.com>
2020-07-02 15:35:59 +0530

    Task 4 complete

--- assignment.js
+++ assignment.js
@@ -2,7 +2,6 @@ const sayHello = name => {
   console.log('Hi ' + name);
 }
 
-///////////////////// Task 3 complete
 const sayHello1 = (name, phrase = 'Hi') => {
   console.log(`${phrase} ${name}`);
 }
@@ -13,7 +12,24 @@ const sayHello2 = () => {
 
 const sayHello3 = name => 'Hi ' + name;
 
+//////////////// Task 4 complete
+const checkInput = (emptyHandler, ...strs) => {
+  let checkEmpty = false;
+  for (str of strs) {
+    if (!str) {
+      checkEmpty = true;
+    }
+  }
+
+  if (checkEmpty) {
+    emptyHandler();
+  }
+};
+
+const displayMessage = () => alert('One of the strings in your list is empty!!');
+
 sayHello('Satx');
 sayHello1('Satx', 'Hello');
 sayHello2();
 console.log(sayHello3('Satx'));
+checkInput(displayMessage, 'fg', 's', 'a', '', 'san', 'tanu');
