d83d8f0d127bfa7273b07f372a14bd0d60daf2cb
Satadru Roy <satadru.roy98@gmail.com>
2020-07-02 16:02:46 +0530

    Some changes made in the code to follow my instructor

--- assignment.js
+++ assignment.js
@@ -1,35 +1,40 @@
-const sayHello = name => {
-  console.log('Hi ' + name);
-}
+///////////////// SOME CHANGES MADE FOR FOLLOWING INSTRUCTOR
+const sayHello = (name) => console.log('Hi ' + name);
 
-const sayHello1 = (name, phrase = 'Hi') => {
-  console.log(`${phrase} ${name}`);
-}
+const sayHello2 = (name, phrase) => console.log(phrase + ' ' + name);
+const sayHello3 = () => console.log('Hi Hard-coded!');
+const sayHello4 = (name) => 'Hi ' + name;
 
-const sayHello2 = () => {
-  console.log('Hi Satx!');
-}
+sayHello('Max');
+sayHello2('Max', 'Hello');
+sayHello3();
+console.log(sayHello4('Max'));
 
-const sayHello3 = name => 'Hi ' + name;
+const sayHello5 = (name, phrase = 'Hi') => console.log(phrase + ' ' + name);
 
-//////////////// Task 4 complete
-const checkInput = (emptyHandler, ...strs) => {
-  let checkEmpty = false;
-  for (str of strs) {
-    if (!str) {
-      checkEmpty = true;
+sayHello5('Manuel');
+sayHello5('Manuel', 'Hello');
+
+function checkInput(cb, ...strings) {
+  let hasEmptyText = false;
+  for (const text of strings) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
     }
   }
 
-  if (checkEmpty) {
-    emptyHandler();
+  if (!hasEmptyText) {
+    cb();
   }
-};
+}
 
-const displayMessage = () => alert('One of the strings in your list is empty!!');
-
-sayHello('Satx');
-sayHello1('Satx', 'Hello');
-sayHello2();
-console.log(sayHello3('Satx'));
-checkInput(displayMessage, 'fg', 's', 'a', '', 'san', 'tanu');
+checkInput(
+  () => {
+    console.log('All not empty!');
+  },
+  'Hello',
+  '12',
+  'adsfa',
+  'Not empty'
+);
